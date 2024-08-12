a68001fa6a4c2053ccaa8d26334b54f437aa61e0
AnaGab21 <ana.gabriellyde.lima@escola.pr.gov.br>
2024-08-12 23:11:25 +0000

    12/08/24-2

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const idade = 17;
+let idade = 17;
 const anoNascimento = 2006;
 let anoAtual = 2024;
+const nomeCompleto = "Ana Gabrielly de Lima";
+
+console.log("Meu nome é " + Ana Gabrielly de Lima + "nascido no ano de " + anoNascimento + " e no ano atual " + 2024 + " completo " + 17 + " anos");
 
 anoAtual = anoAtual + 1;
 
 console.log(anoAtual);
+
+idade = idade + 1;
+console.log(idade);
