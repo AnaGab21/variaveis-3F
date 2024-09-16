a8c2b4671bba4c16d89278e619a4cee910975e6a
AnaGab21 <ana.gabriellyde.lima@escola.pr.gov.br>
2024-09-16 23:06:44 +0000

    16/09/24

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-let idade = 17;
+const prompt = require('prompt-sync');
+const entrada = prompt();
+
 const anoNascimento = 2006;
 let anoAtual = 2024;
 const nomeCompleto = "Ana Gabrielly de Lima";
 
-console.log("Meu nome é " + nomeCompleto + " nascido no ano de " + anoNascimento + " e no ano atual " + 2024 + " completo " + 17 + " anos");
+console.log("Meu nome é " + nomeCompleto + " nascido no ano de " + anoNascimento + " e no ano atual " + 2024 + " completo " + 18 + " anos");
 
 anoAtual = anoAtual + 1;
 
 console.log(anoAtual);
-
-idade = idade + 1;
-console.log(idade);
 
 const listaDeDestinos = new Array(
   " Buenos Aires",
@@ -19,13 +18,15 @@ const listaDeDestinos = new Array(
   " Paris",
   " Nova York"
 );
-
-const idadeComprador = 18;
+var nomeComprador = entrada("Digite seu nome: ");
+var idadeComprador = entrada("Qual a sua idade?");
 
 if(idadeComprador >= 18) {
+  console.log ("Olá senhor(a) " + nomeComprador);
   console.log("Comprador maior de idade");
   console.log("destinos disponiveis: " + listaDeDestinos);
 } else {
+  console.log ("Olá senhor(a) " + nomeComprador);
   console.log("Não foi possivel concluir a sua compra");
   console.log("Indisponivel para menor de idade");
 }
