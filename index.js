d0339d623f80f4ae148745f402de57c363fc0964
AnaGab21 <ana.gabriellyde.lima@escola.pr.gov.br>
2024-09-23 23:00:06 +0000

    23/09/24

--- index.js
+++ index.js
@@ -24,9 +24,23 @@ var idadeComprador = entrada("Qual a sua idade?");
 if(idadeComprador >= 18) {
   console.log ("Olá senhor(a) " + nomeComprador);
   console.log("Comprador maior de idade");
-  console.log("destinos disponiveis: " + listaDeDestinos);
-} else {
+  var contador = 0;
+  while(contador <9 ){
+    console.log(listaDeDestinos[contador]);
+    contador++;
+  
   console.log ("Olá senhor(a) " + nomeComprador);
   console.log("Não foi possivel concluir a sua compra");
   console.log("Indisponivel para menor de idade");
 }
+
+var contador = 0;
+while(contador <9 ){
+  console.log(listaDeDestinos[contador]);
+  contador++;
+}
+  var destino = entrada("Digite o numero que corresponde ao destino selecionado (0-8)");
+  console.log(`destino selecionado foi:
+  ${listaDeDestinos[destino]}`);
+}
+
